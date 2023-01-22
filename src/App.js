a2d6ac76671cf69b53e002ad84d4d2ae3bf2c3fe
SifatNiloy <sifatniloy18@gmail.com>
2023-01-21 19:01:58 -0800

    footer added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Shared/Navbar/Navbar';
+import Footer from './components/Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
 
      <Navbar></Navbar>
-    <h2 className='text-primary text-3xl'>primary checking</h2>
+      <h2 className='text-primary font-bold text-3xl'>primary color checking</h2>
+      <Footer></Footer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   content: [
     "./node_modules/flowbite/**/*.js"
   ],
-  theme: {
-    extend: {
-      colors:{
-        'primary': '#008ab7'
-          
-      }
+  theme: {    
+    colors: {
+      primary: "#0FCFEC",
+      secondary: "#19D3AE",
+      accent: "#3A4256",
+      neutral: "#3D4451",
+      base: "#FFFFFF",
+      info: "#3ABFF8",
+      success: "#36D399",
+      warning: "#FBBD23",
+      error: "#F87272",
     },
   },
   plugins: [
